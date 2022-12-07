e4d1e3630811e6b0ca34c25b3e529e43d2e5b1ab
BetterCallAle <dap22091998@gmail.com>
2022-12-07 16:02:06 +0100

    added the genres in a separated array

--- db.php
+++ db.php
@@ -1,11 +1,13 @@
 <?php
 require_once __DIR__ . "./Models/Movies.php";
 
-$first_movie = new Movies("Forrest Gump", 1994, "en", ["drammatico", "commedia"], "Robert Zamekis", "Tom Hanks", "forrest-gump.jpg");
+$genres = ["drammatico", "commedia", "animazione", "fantastico", "fantascienza", "avventura"];
+
+$first_movie = new Movies("Forrest Gump", 1994, "en", [$genres[0], $genres[1]], "Robert Zamekis", "Tom Hanks", "forrest-gump.jpg");
 $first_movie->setImgPath();
 $first_movie->getImgPath();
 
-$second_movie = new Movies("Spider-Man Into The Spider-Verse", 2018, "en", ["animazione", "fantastico", "fantascienza", "commedia", "avventura"], ["Bob Persicatti", "Peter Ramsey", "Rodney Rothman"], "Shameik Moore", "spider-man.jpg");
+$second_movie = new Movies("Spider-Man Into The Spider-Verse", 2018, "en", [$genres[2], $genres[3], $genres[4], $genres[1], $genres[5]], ["Bob Persicatti", "Peter Ramsey", "Rodney Rothman"], "Shameik Moore", "spider-man.jpg");
 $second_movie->setImgPath();
 $second_movie->getImgPath();
 
