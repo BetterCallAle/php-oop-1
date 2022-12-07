712d9ac3157d99fc1e3121a9f80042ebc1e96e03
BetterCallAle <dap22091998@gmail.com>
2022-12-07 15:44:09 +0100

    class Movies creted and created its first instance

--- Models/Movies.php
+++ Models/Movies.php
@@ -1,3 +1,39 @@
 <?php
 
+/**
+ * class Movies
+ * defines Movies class
+ * @author Alessio Dileo
+ */
+
+class Movies {
+    public $title;
+    public $year;
+    public $original_lang;
+    public $genre;
+    public $director;
+    public $actors;
+    public $img_name;
+    private $img_path;
+
+    function __construct($title, $year, $original_lang, $genre, $director, $actors, $img_name = "")
+    {
+        $this->title = $title;
+        $this->year = $year;
+        $this->original_lang = $original_lang;
+        $this->genre = $genre;
+        $this->director = $director;
+        $this->actors = $actors;
+        $this->img_name = $img_name;
+    } 
+
+    public function setImgPath(){
+        $this->img_path = "img/$this->img_name";
+    }
+
+    public function getImgPath(){
+        return $this->img_path;
+    }
+}
+
 ?>
--- db.php
+++ db.php
@@ -1,2 +1,8 @@
 <?php
+require_once __DIR__ . "./Models/Movies.php";
+
+$first_movie = new Movies("Forrest Gump", 1994, "en", "drammatico", "Robert Zamekis", "Tom Hanks");
+
+var_dump($first_movie);
+
 ?>
