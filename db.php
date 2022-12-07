65de7e9b705216f82ee1d1a0abd375a71b5e3244
BetterCallAle <dap22091998@gmail.com>
2022-12-07 15:51:41 +0100

    created a second Movies instance

--- db.php
+++ db.php
@@ -1,8 +1,16 @@
 <?php
 require_once __DIR__ . "./Models/Movies.php";
 
-$first_movie = new Movies("Forrest Gump", 1994, "en", "drammatico", "Robert Zamekis", "Tom Hanks");
+$first_movie = new Movies("Forrest Gump", 1994, "en", "drammatico", "Robert Zamekis", "Tom Hanks", "forrest-gump.jpg");
+$first_movie->setImgPath();
+$first_movie->getImgPath();
+
+$second_movie = new Movies("Spider-Man Into The Spider-Verse", 2018, "en", "animazione", "Bob Persicatti", "Shameik Moore", "spider-man.jpg");
+$second_movie->setImgPath();
+$second_movie->getImgPath();
 
 var_dump($first_movie);
+echo "-----------";
+var_dump($second_movie);
 
 ?>
